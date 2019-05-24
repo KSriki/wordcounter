fc0827886b699f490dc941c4366c64f3c697b714
Srikant Kumar Kalaputapu <skalaputapu@gmail.com>
2019-05-24 15:00:59 -0400

    finished word count and added about page

--- wordcounter/urls.py
+++ wordcounter/urls.py
@@ -19,6 +19,10 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
+
+    path('', views.home, name='home'),
+
+    path('about/', views.about, name='about'),
+
     path('count/', views.count, name='count')
 ]
